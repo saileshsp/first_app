9890e57c1c0a601e35c1920f68b6ad9f901934cc
PRABHALIKA <Prabhalika999@gmail.com>
2015-05-29 16:01:54 +0530

    ac5

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,6 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.force_ssl = true
 
 module SampleApp
   class Application < Rails::Application
