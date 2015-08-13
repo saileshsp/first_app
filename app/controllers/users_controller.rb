f7e0cbcedbca2d4d2ec63257360aa69e14c1e89a
saileshsp <seera.sailesh1@tcs.com>
2015-08-13 12:04:44 +0530

    css changed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
       before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
      before_action :correct_user,   only: [:edit, :update]
      before_action :admin_user,     only: :destroy
-     acts_as_voter
+     
    def index
     @users = User.paginate(page: params[:page])
    end
